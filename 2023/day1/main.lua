c3e3adffa96e8da449d237b4f7412175acf009ff
johannes-el <Johannes.Elsing@gmx.de>
2025-07-02 21:54:22 +0200

    added a part

--- 2023/day1/main.lua
+++ 2023/day1/main.lua
@@ -20,6 +20,28 @@ end
 local file = "./input.txt"
 local lines = lines_from(file)
 
+local first_digit = 0
+local last_digit = 0
+
+local sum = 0
+
 for _, v in pairs(lines) do
-	print(v)
+	for i = 0, #v do
+		local char = v:sub(i, i)
+		if char:match("%d") then
+			first_digit = char
+			break
+		end
+	end
+
+	for i = #v, 1, -1 do
+		local char = v:sub(i, i)
+		if char:match("%d") then
+			last_digit = char
+			break
+		end
+	end
+	sum = sum + tonumber(first_digit .. last_digit)
 end
+
+print(sum)
